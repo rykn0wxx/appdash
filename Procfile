f52f2d5480f868b98dd238d033ac0e3dbe6dd084
rykn0wxx <raurici666@gmail.com>
2018-12-31 13:06:56 -0500

    base client

---
+++ Procfile
@@ -0,0 +1,1 @@
+client: bash -c 'cd ./client/ && yarn serve'
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap>
+      <v-flex xs12>
+        <h1>Hello World</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
